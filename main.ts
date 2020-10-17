95782134963fd763b67cc24a395e0c6cee85de70
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-17 11:02:17 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,54 +1,58 @@
-radio.onReceivedStringDeprecated(function (receivedString) {
-    if (receivedString == "U") {
+ function doDemo() {
+doPrawo(20)
+}
+ 
+ radio.onReceivedStringDeprecated(function (receivedString) {
+    if (receivedString == "U") { //do gora
         if (debug) {
             led.plot(0, 0)
             led.plot(0, 1)
             led.plot(0, 2)
         }
         step1 = 5
-    } else if (receivedString == "D") {
+    } else if (receivedString == "D") { //w dol
         if (debug) {
             led.plot(0, 2)
             led.plot(0, 3)
             led.plot(0, 4)
         }
         step1 = -5
-    } else if (receivedString == "L") {
+    } else if (receivedString == "L") { //w lewo
         if (debug) {
             led.plot(2, 2)
             led.plot(1, 2)
             led.plot(0, 2)
         }
         step0 = 5
-    } else if (receivedString == "R") {
+    } else if (receivedString == "R") { //w prawo
         if (debug) {
             led.plot(2, 2)
             led.plot(3, 2)
             led.plot(4, 2)
         }
         step0 = -5
-    } else if (receivedString == "1") {
+    } else if (receivedString == "1") { //do przodu
         if (debug) {
             led.plot(4, 0)
             led.plot(4, 1)
             led.plot(4, 2)
         }
         step2 = 5
-    } else if (receivedString == "3") {
+    } else if (receivedString == "3") { //do tylu
         if (debug) {
             led.plot(4, 2)
             led.plot(4, 3)
             led.plot(4, 4)
         }
         step2 = -5
-    } else if (receivedString == "2") {
+    } else if (receivedString == "2") { //szczeka otworz
         if (debug) {
             led.plot(2, 0)
             led.plot(1, 0)
             led.plot(0, 0)
         }
         step3 = 5
-    } else if (receivedString == "4") {
+    } else if (receivedString == "4") { //szczeka zamknij
         if (debug) {
             led.plot(2, 0)
             led.plot(3, 0)
@@ -82,6 +86,7 @@ radio.onReceivedStringDeprecated(function (receivedString) {
         if (debug) {
             basic.showString("A")
         }
+        doDemo()
     } else if (receivedString == "B") {
         if (debug) {
             basic.showString("B")
@@ -103,16 +108,32 @@ let pos0 = 90
 let pos1 = 40
 let pos2 = 120
 let pos3 = 180
-Servo.Servo(0, pos0)
-Servo.Servo(1, pos1)
-Servo.Servo(2, pos2)
-Servo.Servo(3, pos3)
+let pauza = 100
+function doPrawo(ile:number) {
+    for(let i = 0; i < ile; i++) {
+        pos0 += step0
+        pos0 = Math.max(pos0, 0)
+        pos0 = Math.min(pos0, 180),
+        Servo.Servo(0, pos0)
+        basic.pause(pauza)
+    }
+}
+
+
+
+Servo.Servo(0, pos0) //prawo-lewo
+Servo.Servo(1, pos1) //szczeka otworz-zamknij
+Servo.Servo(2, pos2) //gora-dol
+Servo.Servo(3, pos3) //przod-tyl
+basic.showIcon(IconNames.Heart)
+basic.pause(2000)
+basic.clearScreen()
 basic.forever(function () {
     led.plotBrightness(2, 4, 16)
     if (step0 != 0) {
         pos0 += step0
         pos0 = Math.max(pos0, 0)
-        pos0 = Math.min(pos0, 180)
+        pos0 = Math.min(pos0, 180),
         Servo.Servo(0, pos0)
     }
     if (step1 != 0) {
